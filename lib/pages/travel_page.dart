9e6027e482bcd406ef3ea5a708dca48a734a2810
webLXM <1944754630@qq.com>
2024-06-01 18:20:07 +0800

    style(travel): TabBar+TabBarView实现旅拍可滑动切换多Tab

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:trip_flutter/dao/travel_dao.dart';
 import 'package:trip_flutter/model/travel_category_model.dart';
@@ -11,32 +9,58 @@ class TravelPage extends StatefulWidget {
   State<TravelPage> createState() => _TravelPageState();
 }
 
-class _TravelPageState extends State<TravelPage> {
+class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
   List<TravelTab> tabs = [];
   TravelCategoryModel? travelCategoryModel;
+  late TabController _controller;
+
+  get _tabBar => TabBar(
+      controller: _controller,
+      isScrollable: true, // 可以滚动
+      tabAlignment: TabAlignment.start, // 从起始位置开始排列tab
+      indicator: const BoxDecoration(
+          border:
+              Border(bottom: BorderSide(color: Color(0xff2fcfbb), width: 2.0))),
+      indicatorSize: TabBarIndicatorSize.tab,
+      labelColor: Colors.black,
+      tabs: tabs.map<Tab>((TravelTab tab) {
+        return Tab(text: tab.labelName);
+      }).toList());
 
   @override
   void initState() {
     super.initState();
+    _controller = TabController(length: 0, vsync: this);
     getState();
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose(); // 释放控制器占用的资源
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //获取刘海屏Top安全边距
+    double top = MediaQuery.of(context).padding.top;
+
     return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.blue,
-            centerTitle: true,
-            titleTextStyle: const TextStyle(color: Colors.white, fontSize: 22),
-            title: const Text('旅拍')),
         body: Column(
-          children: [Text(jsonEncode(travelCategoryModel))],
-        ));
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: top + 15),
+          color: Colors.white,
+          child: _tabBar,
+        )
+      ],
+    ));
   }
 
   getState() async {
     try {
       TravelCategoryModel? model = await TravelDao.getCategory();
+      _controller = TabController(length: model?.tabs.length ?? 0, vsync: this);
       setState(() {
         tabs = model?.tabs ?? [];
         travelCategoryModel = model;
