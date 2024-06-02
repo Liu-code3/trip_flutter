a22daebd4df6e7ef31827be824204da0a66e6c9e
webLXM <1944754630@qq.com>
2024-06-02 17:14:16 +0800

    style(util): 封装H5跳转函数

--- lib/util/navifator_util.dart
+++ lib/util/navifator_util.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:trip_flutter/pages/home_page.dart';
 import 'package:trip_flutter/pages/login_page.dart';
+import 'package:trip_flutter/widget/hi_webview.dart';
 
 class NavigatorUtil {
-  ///用于在获取不到context的地方, 如 dao 中跳转页面时使用, 需要在HomePage赋值
+  ///用于在获取不到context的地方, 如 dao 中跳转页面时使用, 需要在TabNavigator赋值
+  ///注意: 若TabNavigator被销毁，_context将无法使用
   static BuildContext? _context;
 
   static updateContext(BuildContext context) {
     NavigatorUtil._context = context;
-    print('init: $context');
   }
 
   /// 跳转到指定页面
@@ -39,5 +40,32 @@ class NavigatorUtil {
       // 退出app
       SystemNavigator.pop();
     }
+  }
+
+  ///跳转h5页面
+  static jumpH5(
+      {BuildContext? context,
+      required String url,
+      String? title,
+      bool? hideAppBar,
+      String? statusBarColor}) {
+    BuildContext? safeContext;
+    if (context != null) {
+      safeContext = context;
+    } else if (_context?.mounted ?? false) {
+      safeContext = _context;
+    } else {
+      debugPrint('context is null jumpH5 failed.');
+    }
+
+    Navigator.push(
+        safeContext!,
+        MaterialPageRoute(
+            builder: (context) => HiWebView(
+                  url: url,
+                  title: title,
+                  hideAppBar: hideAppBar,
+                  statusBarColor: statusBarColor,
+                )));
   }
 }
